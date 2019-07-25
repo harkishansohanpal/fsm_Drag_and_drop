c643b76af4a1d8b7d3c794519296b23c86e542ad
aeselcuk <33074339+aeselcuk@users.noreply.github.com>
2019-07-25 09:47:52 -0400

    from ali update

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,68 +1,82 @@
 $(init);
 function init() {
-    var diagram = [];
-    var canvas = $(".canvas");
-    var tools = $(".tools");
-    $(".tool").draggable({
-        helper: "clone"
-    });
-    canvas.droppable({
-        drop: function(event, ui) {
-            var node = {
-                _id: (new Date).getTime(),
-                position: ui.helper.position()
-            };
-            node.position.left -= canvas.position().left;
-            if(ui.helper.hasClass("state-container")){
-                node.type = "state";
-            } else if(ui.helper.hasClass("tool-2")){
-                node.type = "TOOL-2";
-            } else if(ui.helper.hasClass("tool-3")){
-                node.type = "TOOL-3";
-            } else {
-                return;
-            }
-            diagram.push(node);
-            renderDiagram(diagram, node._id);
-        }
-    });
-    function renderDiagram(diagram, _id) {
-        canvas.empty();
-        for(var d in diagram) {
-            var node = diagram[d];
-            var html = "";
-            if(node.type === "state") {
-                
-                html = `<div class="tool tool-1 state-container-${_id} state-container-oncanvas"><div class="state-container-title"><h6 >State</h6></div><div class="state-container-body">                
-                            </div></div>`;
-            } else if(node.type === "TOOL-2") {
-                html = "<h3>TOOL 2</h3>";
-            } else if(node.type === "TOOL-3") {
-                html = "<h3>TOOL 3</h3>";
-            }
-            var dom = $(html).css({
-                "position": "absolute",
-                "top": node.position.top,
-                "left": node.position.left
-            }).draggable({
-                stop: function(event, ui) {
-                    console.log(ui);
-                    var id = ui.helper.attr("id");
-                    for(var i in diagram) {
-                        if(diagram[i]._id == id) {
-                            diagram[i].position.top = ui.position.top;
-                            diagram[i].position.left = ui.position.left;
-                        }
-                    }
-                }
-            }).attr("id", node._id);
-            canvas.append(dom);
-        }
-        $(function(){
-            $(".state-container-oncanvas").resizable();
-            console.log("here in resize")
-        });
-    }
+  var diagram = [];
+  var canvas = $(".canvas");
+  $(".state-container").draggable({
+    helper: "clone"
+  });
+  $(".behaviour").draggable({
+    helper: "clone"
+  });
 
-    
+  var stateID = 0;
+  var behaviourID = 0;
+  canvas.droppable({
+    drop: function(event, ui) {
+      if (ui.helper.hasClass("state-container")) {
+        var state = {
+          _id: stateID++,
+          position: ui.helper.position(),
+          type: "state"
+        };
+      } else if (ui.helper.hasClass("behaviour")) {
+        var behaviour = {
+          _id: stateID++,
+          position: ui.helper.position(),
+          type: "behaviour"
+        };
+      } else if (ui.helper.hasClass("tool-3")) {
+        var event = {
+          _id: eventID++,
+          position: ui.helper.position(),
+          type: "event"
+        };
+      } else {
+        return;
+      }
+      diagram.push(state);
+      renderDiagram(diagram, state._id);
+    }
+  });
+  function renderDiagram(diagram, _id) {
+    canvas.empty();
+    for (var d in diagram) {
+      var state = diagram[d];
+      var html = "";
+      if (state.type == "state") {
+        html = `<div class="state-container-${_id} state-container-oncanvas"><div class="state-container-title"><h6 >State ${
+          state._id
+        }</h6></div><div class="state-container-body">                
+                            </div></div>`;
+      } else if (state.type === "behaviour") {
+        html = '<h6 class="behaviour"></h6>';
+      } else if (state.type === "TOOL-3") {
+        html = "<h3>TOOL 3</h3>";
+      }
+      var dom = $(html)
+        .css({
+          position: "absolute",
+          top: state.position.top,
+          left: state.position.left
+        })
+        .draggable({
+          stop: function(event, ui) {
+            console.log(ui);
+            var id = ui.helper.attr("id");
+            for (var i in diagram) {
+              if (diagram[i]._id == id) {
+                diagram[i].position.top = ui.position.top;
+                diagram[i].position.left = ui.position.left;
+              }
+            }
+          },
+          containment: "parent"
+        })
+        .attr("id", state._id);
+      canvas.append(dom);
+    }
+    $(function() {
+      $(".state-container-oncanvas").resizable();
+    });
+  }
 }
