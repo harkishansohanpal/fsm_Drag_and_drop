ffe0dd9e2d0abded6835b090ce29ea1d820c6e15
harkishansohanpal <harkishansohanpal@gmail.com>
2019-07-27 10:25:19 -0400

    updated events. Improvemnets needed

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,82 +1,263 @@
+
+
+
+var connections = [];
+var context =document.getElementById("theCanvas").getContext("2d");
+var diagram = null;
+
+var addConnectionState = "not begin";
+function addConnection(data){
+	if(data == "start"){
+		
+		addConnectionState = [];
+		console.log(addConnectionState);
+	}
+	if(typeof(data) == "number"){
+		addConnectionState.push(data)
+		if(addConnectionState.length == 2){
+			addConnectionState.push(prompt("enter the color"));
+			connections.push(addConnectionState);
+			drawLines();
+			addConnectionState = "not begin";
+		}
+
+
+	}
+}
+
+
+function drawLine(x0, y0, x1, y1, color){
+	//console.log("drawing line")
+	//console.log(x0, y0, x1, y1)
+	 context.beginPath(); 
+  // Staring point (10,45)
+   context.moveTo(x0,y0);
+  // End point (180,47)
+  context.lineTo(x1,y1);
+  context.strokeStyle = color;
+  // Make the line visible
+  context.stroke();
+}
+function drawCircle(x,y,r){
+	console.log(x,y,r)
+	context.beginPath();
+	context.arc(x,y,r,0*Math.PI,2*Math.PI);
+	context.stroke();
+	
+}
+function getLocation(i){
+	return diagram[i].position;
+}
+function connect(x,y){	
+	drawLine(getLocation(x).left+150, getLocation(x).top+150, getLocation(y).left+150, getLocation(y).top+150);
+}
+function connectMultiplePairs(lst1, lst2){
+	context.clearRect(0,0,3000, 3000);
+	for(var i=0;i<lst1.length;i++){
+		connect(lst1[i] , lst2[i]);
+	}
+}
+function getAllLocation(){
+	for(var i=0; i<diagram.length;i++){
+		console.log(i + " " + getLocation(i).left + " " + getLocation(i).top);
+	}
+	
+}
+function drawLines(ui){
+				context.clearRect(0,0,3000,3000);
+				connections.forEach(function(d){
+				var domain = d[0];
+				var target = d[1];
+				var color = d[2];
+				try{
+				var id = getIdFromString(ui.helper[0].getAttribute("class"));
+				} catch(e){
+					var id = null;
+				}
+				if(target == id){
+					try{
+					drawLine(diagram[domain].position.left+150, diagram[domain].position.top+150,ui.position.left+150, ui.position.top+150, color);
+					} catch( e){
+						;
+					}
+				}
+				else if(domain == id){
+					try{
+					drawLine(ui.position.left+150, ui.position.top+150,diagram[target].position.left+150, diagram[target].position.top+150, color);
+					} catch( e){
+						;
+					}				
+				} else{
+					try{
+					drawLine(diagram[domain].position.left+150, diagram[domain].position.top+150 , diagram[target].position.left+150, diagram[target].position.top+150, color);
+					} catch( e){
+						;
+					}						
+					
+				}
+				})
+	
+}
+//state-container-oncanvas State-2 ui-draggable ui-draggable-handle ui-droppable
+
+function getIdFromString(s){
+		//console.log("ID");
+		//console.log(s);
+		return parseInt((s.substr(31, 1)));
+}
+
+function clickState(){
+	
+	addConnection("start");
+}
+
+
+
+/*============================================================================================================================================= */
 $(init);
 function init() {
-  var diagram = [];
+
+  //diagram is the main array, we push data into it
+  diagram = [];
   var canvas = $(".canvas");
-  var stateCanvasBody = $(".state-container");
-
-
+  var stateCanvasBody = $(".state-container-oncanvas");
+  context = $("#theCanvas")[0].getContext("2d");
+//make state container draggable
   $(".state-container").draggable({
     helper: "clone",
   });
-  $(".behaviour").droppable({
+
+//make behaviour draggable
+  $(".behaviour").draggable({
     helper: "clone",
   });
 
+  //Initialize state-id and behaviour-id
   var stateID = 0;
   var behaviourID = 0;
+
+  //make canvas droppable
   canvas.droppable({
-    accept:".state-container,.behaviour",
+    // canvas can divs with below class
+    accept:".state-container", 
+    //function executed after drop event in canvas
+    //check if it is state-container else return
+    // if dropped div is state-container then make object state
     drop: function(event, ui) {
+	drawLines();
     if (ui.helper.hasClass("state-container")) {
         var state = {
           _id: stateID++,
           position: ui.helper.position(),
+          behaviour:[],
           type: "state"
         };
+		state.position.left -= 300;
       } else {
         return;
       }
+
+      //push state to diagram array
       diagram.push(state);
-      renderDiagram(diagram, state._id);
+
+      //call render function and pass diagram array and state-id
+      renderStateContainer(diagram, state._id);
     }
   });
 
- 
 
-/*================================================render function=======================================================*/
-
-
-  function renderDiagram(diagram, _id) {
+  
+  function renderStateContainer(diagram, _id) {
     canvas.empty();
+    //loop through diagram array
     for (var d in diagram) {
       var state = diagram[d];
       var html = "";
-      var htmlBehaviour = "";
+      
+      //if state.type is state then declare html and 
       if (state.type == "state") {
-        html = `<div class="state-container-oncanvas">
+        html = `<div class="state-container-oncanvas State-${state._id}">
                     <div class="state-container-title">
-                        <h6 >State ${state._id}</h6>
+                        <h6 >State-${state._id}</h6>
                     </div>
                     <div class="state-container-body state-container-body-oncanvas">                
                     </div>
                 </div>`;
-      } else if (state.type === "behaviour") {
-        htmlBehaviour = '<h6 class="behaviour"></h6>';
-      } else if (state.type === "TOOL-3") {
-        html = "<h3>TOOL 3</h3>";
-      }
-      var dom = $(html)
+		//console.log(diagram[d]["_id"] + "," + d)
+		//console.log( + " " + state.position.top+ " " + state.position.left + " not drag")
+	    var dom = $(html)
         .css({
           position: "absolute",
           top: state.position.top,
           left: state.position.left
         })
+        //make state-container in canvas draggable
         .draggable({
-          stop: function(event, ui) {
-            console.log(ui);
-            var id = ui.helper.attr("id");
+			drag:function(event, ui){
+				//console.log(event)
+				drawLines(ui);
+			},
+			stop: function(event, ui) {
+            var id = getIdFromString(ui.helper[0].getAttribute("class") );
             for (var i in diagram) {
+				//console.log(id)
               if (diagram[i]._id == id) {
+				 var state = diagram[d]; 
                 diagram[i].position.top = ui.position.top;
                 diagram[i].position.left = ui.position.left;
+				//console.log(id + " " + state.position.top + " " + state.position.left+ " drag")
               }
             }
           },
           containment: "parent"
         })
-        
-        .attr("id", state._id);
+        //make new state-container droppable
+        //and accept the behaviour div only
+        .droppable({  
+          accept:".behaviour",
+          drop: function(event, ui){
+			  
+            // console.log($(this));
+            // var stateContainerId = $(this).attr("id");
+              //console.log(ui);
+              if (ui.helper.hasClass("behaviour")) {
+                  var behaviour = {
+                      _id: behaviourID++,
+                      position: ui.helper.position(),
+                      type: "behaviour"
+                  };
+              }
+              diagram.push(behaviour);
+              // console.log(stateCanvasBody);
+			  console.log(ui);
+              var htmlBehaviour = `<h6  class="behaviour"
+              >${ui.helper["0"].innerHTML}
+              </h6>`;
+              $(".state-container-body-oncanvas",this).append(htmlBehaviour);
+              
+               //$(this)["0"].childNodes[3].$(".state-container-body-oncanvas").append("htmlBehaviour");
+          }
+      })
+		.click(function(e, ui){
+			//console.log(e);
+			id = getIdFromString(e.currentTarget.getAttribute("class"));
+			console.log(id);
+			addConnection(id);
+	  })
+        canvas.append(dom);
 
+      } else if (state.type === "behaviour") {
+        
+
+      } else if (state.type === "TOOL-3") {
+        html = "<h3>TOOL 3</h3>";
+      }
+    }
+  }
+
+
+  function renderBehaviour(){
+    htmlBehaviour = '<h6 class="behaviour "></h6>';
 
         var dom2 = $(htmlBehaviour)
         .css({
@@ -84,13 +265,7 @@ function init() {
           top: state.position.top,
           left: state.position.left
         });
-        
-        canvas.append(dom);
 
-    }
-
+      stateCanvasBody.append(dom2);
   }
-
-  
-  
 }
